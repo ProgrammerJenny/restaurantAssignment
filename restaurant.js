660baa9dd9bfe7b861de391002f961c25f67c30d
Jennifer.Trongard <jennifer.trongard@gmail.com>
2022-02-27 22:42:41 -0600

    assigbment 3 contact us page #7.

--- restaurant.js
+++ restaurant.js
@@ -1,6 +1,43 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    // TODO: Complete the function
+    document.querySelector("#submit").addEventListener("click", clickHandler);
+    document.querySelector("#reset").addEventListener("click", resetFormHandler);
+ }
+
+ function resetFormHandler()
+ {
+    document.getElementById("errorMessage").innerHTML="";
+    document.getElementById("name").value="";
+    document.getElementById("phone").value="";
+    document.getElementById("email").value="";
+    document.getElementById("task").value="Catering";
+    document.getElementById("additionalinfo").value="";
+    document.getElementById("beenHereNo").checked=true;  
+    document.getElementById("M").checked=false;  
+    document.getElementById("T").checked=false;  
+    document.getElementById("W").checked=false;  
+    document.getElementById("Th").checked=false;  
+    document.getElementById("F").checked=false;  
+
+    
+ }
+
+function clickHandler()
+{
+    let myphone = document.getElementById("phone").value; 
+    if(isNaN(myphone) || myphone.length>9 ){
+
+        let myerror= "phone number is not valid.";
+        document.getElementById("errorMessage").innerHTML=myerror;
+     }
+     else{
+        document.getElementById("errorMessage").innerHTML="";
+     }
 
 
-
+}
 
 
 // carousel  
@@ -16,5 +53,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
